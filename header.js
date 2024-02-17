5e3f913f15d5d125d8420e827d1389a2e9403418
LightninG8 <tov.kessler@yanedx.ru>
2024-02-18 02:53:47 +0300

    feat: add header with mobile menu template

---
+++ header.js
@@ -0,0 +1,43 @@
+(() => {
+  document.addEventListener("DOMContentLoaded", () => {
+    const headerElem = document.querySelector(".header");
+    const burgerButtonElem = document.querySelector('.burger');
+
+    const onScroll = () => {
+      // const headerBodyHeight = getComputedStyle(document.documentElement)
+      //   .getPropertyValue("--header-body-height")
+      //   .replace("px", "");
+
+      if (document.documentElement.scrollTop > 199) {
+        headerElem.classList.add("scrollOn");
+      } else {
+        headerElem.classList.remove("scrollOn");
+      }
+    };
+
+    const onResize = () => {
+      console.log(window.innerWidth);
+      console.log();
+
+      if (window.innerWidth < 980) {
+        document.removeEventListener("scroll", onScroll);
+        headerElem.classList.remove("scrollOn");
+
+        document.body.classList.remove('mobileMenu');
+
+      } else {
+        document.addEventListener("scroll", onScroll);
+        onScroll();
+      }
+    };
+
+    window.addEventListener("resize", onResize);
+
+    onResize();
+
+
+    burgerButtonElem.addEventListener("click", () => {
+      document.body.classList.toggle('mobileMenu');
+    })
+  });
+})();
